460f1897279cbfe3c8d96170177de9f68a0a69a9
Miro <MiroPhoenix91@gmail.com>
2025-05-04 02:54:30 +0200

    new

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 
+app.use(express.static('dist'));
+
 app.use(cors());
 
 app.use(express.json());
-app.use(express.static('dist'));
 
 let notes = [
 	{
